da2d3bdf9356d8d10d2e965bb79dd96e29f79ef1
Barbara Helen <dsbarbarahelen@gmail.com>
2023-03-28 18:22:57 -0400

    no message

--- desafio1Delegate/desafio1Delegate/ViewController.swift
+++ desafio1Delegate/desafio1Delegate/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameTextField.placeholder = "Digite seu nome"
+        addressTextField.placeholder = "Digite seu endereço"
+        passwordTextField.placeholder = "Digite sua senha"
+        nameTextField.delegate = self
+        addressTextField.delegate = self
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
+    extension ViewController: UITextFieldDelegate {
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            if textField.hasText == true 
+        }
+        
+}
