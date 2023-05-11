ea227fff8eb05ae403d324a1ce9656581f94202f
Barbara Helen <dsbarbarahelen@gmail.com>
2023-05-11 18:44:12 -0400

    no message

---
+++ notificationCenterEProtocol/notificationCenterEProtocol/SelectionViewController/SelectionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SelectionViewController.swift
+//  notificationCenterEProtocol
+//
+//  Created by Barbara Silva on 2023-05-04.
+//
+
+import UIKit
+
+class SelectionViewController: UIViewController {
+
+    @IBOutlet weak var logoImageView: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ notificationCenterEProtocol/notificationCenterEProtocol/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  notificationCenterEProtocol
+//
+//  Created by Barbara Silva on 2023-05-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var clickButton: UIButton!
+
+    
+    @IBOutlet weak var appleImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func tappedClickButton(_ sender: Any) {
+        lev vc = UIStoryboard(name: String, bundle: <#T##Bundle?#>)
+    }
+    
+    
+}
+
